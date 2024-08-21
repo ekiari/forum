f2249ea8e13cb87a86865b7a3c5af298bb9c3f14
ekiari <edhvkdf@gmail.com>
2024-08-21 16:38:55 +0300

    13

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -13,7 +13,19 @@ export const PostItem = ({ post }) => {
 
     return (
         <div className="flex flex-col basis-1/4 flex-grow">
-            <div>IMAGE</div>
+            <div
+                className={
+                    post.imgUrl ? "flex rounded-sm h-80" : "flex rounded-sm"
+                }
+            >
+                {post.imgUrl && (
+                    <img
+                        src={`http://localhost:3002/${post.imgUrl}`}
+                        alt="img"
+                        className="object-cover w-gull"
+                    />
+                )}
+            </div>
             <div className="flex justify-between items-center pt-2">
                 <div className="text-xs text-white opacity-50">
                     {post.username}
